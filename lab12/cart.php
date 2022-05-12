33f8cc98c8e05a1fafce79db70d6e4c6adf83ed9
Crisss <69833083+cgcris09@users.noreply.github.com>
2022-05-12 15:25:57 -0500

    Add files via upload

---
+++ lab12/cart.php
@@ -0,0 +1,32 @@
+<!--  I honor Parkland's core values by affirming that I have 
+followed all academic integrity guidelines for this work.
+Cristal Cardenas
+201H -->
+
+
+<html>
+<head>
+<title>Welcome Page</title>
+<?php
+
+require("lib/phpfunctions.php");
+?>
+
+
+</head>
+<body>
+
+<?php readfile("lib/header.html"); ?>
+
+<?php echo "place holder"; ?>
+<?php readfile("lib/footer.html"); ?>
+
+</body>
+
+
+
+
+
+
+
+</html>
---
+++ lab12/item1.php
@@ -0,0 +1,32 @@
+<!--  I honor Parkland's core values by affirming that I have 
+followed all academic integrity guidelines for this work.
+Cristal Cardenas
+201H -->
+
+
+<html>
+<head>
+<title>Welcome Page</title>
+<?php
+
+require("lib/phpfunctions.php");
+
+?>
+</head>
+<body>
+
+<?php readfile("lib/header.html");
+
+echo "this is a place holder for now";
+?>
+
+
+</body>
+
+
+<?php readfile("lib/footer.html"); ?>
+
+
+
+
+</html>
